6bfd75cf06275b9fdf201f1bddbdd05f11652c30
Christy <christyamin22@gmail.com>
2023-02-13 16:20:05 -0800

    starting demo project 1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.53.0"
     }
   }
@@ -13,45 +13,57 @@ provider "aws" {
 
 variable "vpc_cidr_block" {
   description = "vpc cidr block"
-  default = "10.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
   description = "subnet cidr block"
-  default = "10.0.10.0/24"
+  default     = "10.0.10.0/24"
 }
 
 variable "availability_zone" {
   description = "name of availability zone"
-  default = "us-east-1a"
+  default     = "us-east-1a"
 }
 
-variable "environment" {
+variable "env_prefix" {
   description = "name of environment"
-  default = "development"
+  default     = "dev"
 }
 
-resource "aws_vpc" "development-vpc" {
+resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
-   tags = {
-    Name = "development-vpc"
-    Environment = var.environment
+  tags = {
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id     = aws_vpc.development-vpc.id
-  cidr_block = var.subnet_cidr_block
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.availability_zone
   tags = {
-    Name = "${var.environment}-subnet"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
 }
