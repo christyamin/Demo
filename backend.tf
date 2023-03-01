bf6574edae37642c4d00941f898d6c3082031d19
Christy <christyamin22@gmail.com>
2023-03-01 15:19:28 -0800

    starting eks demo project

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "christyamin-app"
-    region = "us-east-1"
-    key    = "modules/terraform.tfstate"
-  }
-}
--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "ec2_public_ip_1" {
-  value = module.myapp-server_1.instance.public_ip
-}
-output "ec2_public_ip_2" {
-  value = module.myapp-server_2.instance.public_ip
-}
